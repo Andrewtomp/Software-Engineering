7c45b344b0673fbde1905faaa08769577a60774e
Johnny Bravo <hakmonkey@gmail.com>
2025-02-10 19:03:05 -0500

    Added `swagger` doc comments

--- front-runner_backend/cmd/main/main.go
+++ front-runner_backend/cmd/main/main.go
@@ -1,4 +1,15 @@
 // main.go
+// @title Front Runner API
+// @version 1.0
+// @description API documentation for the Front Runner application.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@example.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 package main
 
 import (
@@ -7,7 +18,10 @@ import (
 	"log"
 	"net/http"
 
+	_ "front-runner/docs" // This is important for swagger to find your docs!
+
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger" // swagger middleware
 )
 
 const (
@@ -15,9 +29,7 @@ const (
 )
 
 func main() {
-
 	cert, err := tls.LoadX509KeyPair("server.crt", "server.key")
-
 	if err != nil {
 		log.Fatalf("Failed to load X509 key pair: %v", err)
 	}
@@ -27,9 +39,16 @@ func main() {
 	}
 
 	router := mux.NewRouter()
+
+	// API endpoints
 	router.HandleFunc("/register", login.RegisterUser).Methods("POST")
 	router.HandleFunc("/login", login.LoginUser).Methods("POST")
 	router.HandleFunc("/logout", login.LogoutUser).Methods("GET")
+
+	// Serve Swagger UI on /swagger/*
+	router.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
+
+	// Serve static files for your webpage
 	s := http.StripPrefix("/static/", http.FileServer(http.Dir("../../../front-runner/build/static/")))
 	router.PathPrefix("/static/").Handler(s)
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../../../front-runner/build")))
