b1033c8be0b6e0cab50561c53495daeb34e6a33e
Andrewtomp <andrewtomp14@gmail.com>
2025-03-03 15:03:52 -0500

    Add tests for NavBar, ProductForm, LoginForm, and RegistrationForm; remove obsolete App.test.js

---
+++ front-runner/src/components/__tests__/NavBar.Test.jsx
@@ -0,0 +1,26 @@
+// NavBar.test.js
+import { render, fireEvent,screen } from '@testing-library/react';
+import NavBar from '../components/NavBar';
+
+describe('NavBar', () => {
+    it('navigates to the correct links when clicked', () => {
+        // Mock the window.location.href to check navigation
+        const mockLocation = { href: '' };
+        delete window.location;
+        window.location = mockLocation;
+
+        render(<NavBar />);
+        fireEvent.click(screen.getByText(/frontrunner/i));
+        expect(window.location.href).toBe('/');
+        
+        fireEvent.click(screen.getByText(/my products/i));
+        expect(window.location.href).toBe('/products');
+
+        fireEvent.click(screen.getByText(/my storefronts/i));
+        expect(window.location.href).toBe('/storefronts');
+
+        fireEvent.click(screen.getByText(/my orders/i));
+        expect(window.location.href).toBe('/orders');
+
+    });
+});
