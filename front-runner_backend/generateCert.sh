45851b34ed19ea92957917db4336c77e767fd934
Johnny Bravo <hakmonkey@gmail.com>
2025-03-31 04:34:37 -0400

    Update to gen additional key
    Script now also generates the `.storefrontkey` to encrypt the storefront info

--- front-runner_backend/generateCert.sh
+++ front-runner_backend/generateCert.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -sha256 -days 3650 -nodes -subj "/C=US/ST=FL/L=Gainesville/O=FrontRunner/OU=/CN=localhost"
+
+openssl rand -base64 32 > .storefrontkey
